c7f4e9ed60bbbbeab22be792f1c2fd5bd61c8215
Tomas D. <tomasduron1@gmail.com>
2026-04-21 17:38:05 -0700

    Fixing ddragon

--- src/externals/ddragon.js
+++ src/externals/ddragon.js
@@ -7,7 +7,7 @@ export async function getLatestChampionDDragon(language = "en_US") {
 	let response;
 	let versionIndex = 0;
 	do { // I loop over versions because 9.22.1 is broken
-		const version = (await fetch("http://ddragon.leagueoflegends.com/api/versions.json").then(async(r) => await r.json()))[versionIndex++];
+		const version = (await fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(async(r) => await r.json()))[versionIndex++];
 	
 		response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
 	}

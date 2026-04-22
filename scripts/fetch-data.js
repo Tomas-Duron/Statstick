1799fc515c8398aa56eeb7dc21cdfdd40ebd6b3c
Tomas D. <tomasduron1@gmail.com>
2026-04-21 18:17:17 -0700

    Update fetch-data.js

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,27 +1,25 @@
 // scripts/fetch-data.js
 import fs from 'fs/promises';
 
-let language = "en_US"
-let championJson = {};
+const language = "en_US";
 
-if (championJson[language])
-    return championJson[language];
+// Get the latest working version
+const versions = await fetch("https://ddragon.leagueoflegends.com/api/versions.json")
+  .then(r => r.json());
 
 let response;
 let versionIndex = 0;
-do { // I loop over versions because 9.22.1 is broken
-    const version = (await fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(async(r) => await r.json()))[versionIndex++];
 
-    response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
-}
-while (!response.ok)
+do { // Loop over versions because 9.22.1 is broken
+  const version = versions[versionIndex++];
+  response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
+} while (!response.ok);
 
-championJson[language] = await response.json();
-// return championJson[language];
+const championJson = await response.json();
 
 const processed = {
   updatedAt: new Date().toISOString(),
-  values: championJson[language]
+  champions: championJson.data
 };
 
 await fs.writeFile('./data/cached-data.json', JSON.stringify(processed, null, 2));
